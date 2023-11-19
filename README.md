12d687cb9f06807665f5f05cac99e67db3e72b93
Enzo Notario <enzo.notario@gmail.com>
2023-11-19 20:22:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# @es-js/crear-terminal-app
+
+[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://github.com/codespaces/new?hide_repo_select=true&ref=main&repo=583951106)
+
+Plantilla para iniciar un proyecto [EsJS](https://esjs.dev/) en Modo Terminal.
+
+## Uso
+
+### Desde GitHub
+Utilizando este proyecto como *template*.
+
+### Desde tu computadora
+
+Ejecutando:
+
+```
+npx degit https://github.com/es-js/crear-terminal-app mi-esjs-terminal-app
+cd mi-esjs-terminal-app
+pnpm i
+```
---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { URL, fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+import EsJS from '@es-js/vite-plugin-esjs'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    // https://github.com/es-js/esjs
+    EsJS(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+      '.esjs',
+    ],
+  },
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+  },
+})
